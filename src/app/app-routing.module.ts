6de142bee5adc68bca794e4f4994baa88180a870
Samael Martínez <mc.fsamael@gmail.com>
2022-06-18 23:35:42 -0500

    Añadí componentes principales y algo de contenido a inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './contacto/contacto.component';
+import { FaqComponent } from './faq/faq.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'faq', component: FaqComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
