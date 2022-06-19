21385d9b00af2f9f8ce9f5501f4184dc031b6c8c
Samael Martínez <mc.fsamael@gmail.com>
2022-06-18 19:15:08 -0500

    Configurar angularfire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'proyectofinal-fe034',
+    appId: '1:357453608084:web:7a49b6e73e790421cc7081',
+    storageBucket: 'proyectofinal-fe034.appspot.com',
+    apiKey: 'AIzaSyAGUN4_GUvlVfea_EU9JV6fn3wx63a7h10',
+    authDomain: 'proyectofinal-fe034.firebaseapp.com',
+    messagingSenderId: '357453608084',
+  },
   production: false
 };
 
