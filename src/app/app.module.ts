1ae0228ced23fd161fa30075d488004ce96ddda3
Samael Martínez <mc.fsamael@gmail.com>
2022-06-18 21:31:33 -0500

    nuevo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,13 @@ import { providePerformance,getPerformance } from '@angular/fire/performance';
 import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
 import { provideStorage,getStorage } from '@angular/fire/storage';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CompComponent } from './comp/comp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CompComponent
   ],
   imports: [
     BrowserModule,
