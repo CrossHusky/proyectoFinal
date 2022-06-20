257c31d3db580e7faae632611c3b6019122bb7cd
Samael Martínez <mc.fsamael@gmail.com>
2022-06-19 19:30:51 -0500

    Actualicé las rutas de login y registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactoComponent } from './contacto/contacto.component';
 import { FaqComponent } from './faq/faq.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'faq', component: FaqComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegistroComponent },
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
 ];
 
